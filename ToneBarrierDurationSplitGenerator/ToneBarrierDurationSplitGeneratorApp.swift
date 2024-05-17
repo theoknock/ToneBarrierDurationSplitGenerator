8296f5c49cc912acfc2afea7e0fed460b74b7100
James Alan Bush <jamesalanbush1972@gmail.com>
2024-05-17 05:40:49 -0400

    Initial Commit

---
+++ ToneBarrierDurationSplitGenerator/ToneBarrierDurationSplitGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToneBarrierDurationSplitGeneratorApp.swift
+//  ToneBarrierDurationSplitGenerator
+//
+//  Created by Xcode Developer on 5/17/24.
+//
+
+import SwiftUI
+
+@main
+struct ToneBarrierDurationSplitGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
